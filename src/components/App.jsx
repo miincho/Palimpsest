81d39293998399058c75b01f0fa01ea211f97409
chloe yiu <chloeyiu@icloud.com>
2024-09-18 00:09:56 -0400

    goodnight guys
    crisis averted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,89 @@
 // import React from 'react'; //commented it out cause there was an error
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
-import Header from './Header.jsx';
-import C001 from '../sauce/C001.jsx';
-import M001 from '../sauce/M001.jsx';
-import C002 from '../sauce/C002.jsx';
-import M002 from '../sauce/M002.jsx';
-import C003 from '../sauce/C003.jsx';
-import M003 from '../sauce/M003.jsx';
-import './App.css';
+import { BrowserRouter as Router, Link, Routes, Route } from "react-router-dom";
+import Header from "./Header.jsx";
+import C001 from "../sauce/C001.jsx";
+import M001 from "../sauce/M001.jsx";
+import C002 from "../sauce/C002.jsx";
+import M002 from "../sauce/M002.jsx";
+import C003 from "../sauce/C003.jsx";
+import M003 from "../sauce/M003.jsx";
+import C004 from "../sauce/C004.jsx";
+import "./App.css";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <div id="content">
-          <Routes>
-            <Route path="/" element={<Links />} />
-            <Route path="/sauce/C001" element={<C001 />} />
-            <Route path="/sauce/M001" element={<M001 />} />
-            <Route path="/sauce/C002" element={<C002 />} />
-            <Route path="/sauce/M002" element={<M002 />} />
-            <Route path="/sauce/C003" element={<C003 />} />
-            <Route path="/sauce/M003" element={<M003 />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<Header />
+				<div id="content">
+					<Routes>
+						<Route
+							path="/"
+							element={<Links />}
+						/>
+						<Route
+							path="/sauce/C001"
+							element={<C001 />}
+						/>
+						<Route
+							path="/sauce/M001"
+							element={<M001 />}
+						/>
+						<Route
+							path="/sauce/C002"
+							element={<C002 />}
+						/>
+						<Route
+							path="/sauce/M002"
+							element={<M002 />}
+						/>
+						<Route
+							path="/sauce/C003"
+							element={<C003 />}
+						/>
+						<Route
+							path="/sauce/M003"
+							element={<M003 />}
+						/>
+						<Route
+							path="/sauce/C004"
+							element={<C004 />}
+						/>
+					</Routes>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 function Links() {
-  return (
-    <nav>
-      <ul>
-        <li><Link to="/sauce/C001">C001.html</Link></li>
-        <li><Link to="/sauce/M001">M001.html</Link></li>
-        <li><Link to="/sauce/C002">C002.html</Link></li>
-        <li><Link to="/sauce/M002">M002.html</Link></li>
-        <li><Link to="/sauce/C003">C003.html</Link></li>
-        <li><Link to="/sauce/M003">M003.html</Link></li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/sauce/C001">C001.html</Link>
+				</li>
+				<li>
+					<Link to="/sauce/M001">M001.html</Link>
+				</li>
+				<li>
+					<Link to="/sauce/C002">C002.html</Link>
+				</li>
+				<li>
+					<Link to="/sauce/M002">M002.html</Link>
+				</li>
+				<li>
+					<Link to="/sauce/C003">C003.html</Link>
+				</li>
+				<li>
+					<Link to="/sauce/M003">M003.html</Link>
+				</li>
+				<li>
+					<Link to="/sauce/C004">C004.html</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default App;
---
+++ src/sauce/C004.jsx
@@ -0,0 +1,69 @@
+import "../css/C001.css";
+import "../assets/fonts.css";
+import Footer from "../components/Footer.jsx";
+
+function App() {
+	return (
+		<>
+			<div className="content">
+				<div>
+					<h1>HI.</h1>
+					<h2>Wednesday, September 18, 2024.</h2>
+					<p>Hello, Palimpsest. Long time no see. How have you been?</p>
+					<br></br>
+					<p>
+						Since we last met, Mink and I started our senior year at Parsons
+						School of Design! Time flies. I cannot believe this is our last year
+						of undergraduate studies. There is this bittersweet feeling.
+					</p>
+					<br></br>
+					<p>
+						At the moment, I am procrastinating on my homework assignments for
+						American Art (PLAH 4035 - A) and Communication Design Studio: Motion
+						Graphics (PSAM 2440 - A). Okay, I am totally procrastinating.
+						Actually, I take that back. I am simply doing a stimulating mental
+						exercise to keep my mind sharp for the long night ahead. Yeah. It
+						has been a while since I opened up Visual Studio Code. Also, I had
+						some random thoughts I had to jot down to re-focus on annotating. I
+						read the word <em>cincture</em> which distracted me because I had to
+						look up its definition. I have also been meaning to write down a
+						list of scents.
+					</p>
+					<br></br>
+					<h3>List</h3>
+					<ul>
+						<li>
+							notable smells
+							<ul>
+								<li>wood</li>
+								<li>books</li>
+								<li>cardboard boxes</li>
+							</ul>
+						</li>
+						<li>
+							sure, it is a cure
+							<ul>
+								<li>cincture</li>
+								<li>tincture</li>
+							</ul>
+						</li>
+						<li>
+							iced beverages
+							<ul>
+								<li>precipice</li>
+								<li>trepidation</li>
+								<li>rain</li>
+								<li>pour</li>
+								<li>snore</li>
+							</ul>
+						</li>
+					</ul>
+					<br></br>
+					<p>Signed, C ⋆✴︎˚｡⋆</p>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+}
+export default App;
